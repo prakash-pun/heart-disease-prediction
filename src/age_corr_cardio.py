42942d549be792e6528680947cc780d7e5e5dec4
Prakash Pun <poonprakash22@gmail.com>
2024-02-07 14:13:54 -0500

    train and test data wip

--- src/age_corr_cardio.py
+++ src/age_corr_cardio.py
@@ -1,14 +1,15 @@
 # all library import
-import os
 import pandas as pd
+from utils import get_csv_file
 
-current_dir = os.getcwd()
-file_path = os.path.join(current_dir, "data/cardio_data_raw.csv")
+path = get_csv_file("cleaned_data.csv")
 
-df = pd.read_csv(file_path, sep=";")
+df = pd.read_csv(path)
 
 print(df.head(5))
 print(df.info())
 print(df.isnull().head(5))
+print(df.isnull().sum())
 print(df["age"].isnull())
 print(df["age"].isin([0,1]).all())
+print(df.describe())
---
+++ src/split_dataset.py
@@ -0,0 +1,19 @@
+from sklearn.model_selection import train_test_split 
+from utils import get_data
+
+def split_data():
+    df = get_data()
+
+    X = df.drop("cardio", axis = 1)
+    y = df["cardio"] # target 
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state= 42)
+
+    # saving the train data
+    X_train.to_csv("data/train_data.csv", index=False)
+    X_test.to_csv("data/test_data.csv", index=False)
+    y_train.to_csv("data/train_data_labels.csv", index=False, header=True)
+    y_test.to_csv("data/test_data_labels.csv", index=False, header=True)
+
+
+split_data()
