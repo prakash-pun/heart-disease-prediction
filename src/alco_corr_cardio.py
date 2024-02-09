dd4f1b60718ba6ee02d6c7ab4e02ea10c4f33116
Tirth Patel <alphatirthpatel@gmail.com>
2024-02-08 19:25:49 -0500

    updated_week4

--- src/alco_corr_cardio.py
+++ src/alco_corr_cardio.py
@@ -1,16 +1,18 @@
-# all library import
 import os
 import pandas as pd
 
 current_dir = os.getcwd()
-file_path = os.path.join(current_dir, "data/cardio_data.csv")
+file_path = os.path.join("D:\\College\\Assignments\\Steps\\cardiovascular-disease-prediction\\data\\cleaned_data.xlsx")
 
-df = pd.read_csv(file_path, sep=";")
+df = pd.read_excel(file_path)
 
 print(df.head(5))
 print(df.info())
 print(df.isnull().head(5))
-# Alcohol intake | subjective Feature | alco | binary (0, 1)
 print(df["alco"].isnull())
 print(df["alco"].isna().all())
 print(df["alco"].isin([0,1]).all())
+
+from matplotlib import pyplot as plt
+df.plot(kind='scatter', x='active', y='bp_lo')
+plt.gca().spines[['top', 'right',]].set_visible(False)
