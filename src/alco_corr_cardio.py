49482b08d920917fbb54522c55f36a17432abb21
Tirth Patel <alphatirthpatel@gmail.com>
2024-02-08 20:40:17 -0500

    Updated_week4

--- src/alco_corr_cardio.py
+++ src/alco_corr_cardio.py
@@ -2,6 +2,7 @@
 
 import os
 import pandas as pd
+from matplotlib import pyplot as plt
 
 current_dir = os.getcwd()
 file_path = os.path.join("D:\\College\\Assignments\\Steps\\cardiovascular-disease-prediction\\data\\cleaned_data.xlsx")
@@ -16,6 +17,5 @@ print(df["alco"].isnull())
 print(df["alco"].isna().all())
 print(df["alco"].isin([0,1]).all())
 
-from matplotlib import pyplot as plt
 df.plot(kind='scatter', x='active', y='bp_lo')
 plt.gca().spines[['top', 'right',]].set_visible(False)
