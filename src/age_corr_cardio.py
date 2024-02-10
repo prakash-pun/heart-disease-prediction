0ce3f529abdd432212d649bccc15782338373b02
Prakash Pun <poonprakash22@gmail.com>
2024-02-09 19:51:53 -0500

    change dataset with empty column, change folder name

--- src/age_corr_cardio.py
+++ src/age_corr_cardio.py
@@ -2,14 +2,13 @@
 import pandas as pd
 from utils import get_csv_file
 
-path = get_csv_file("cleaned_data.csv")
+file_path = get_csv_file("cleaned_data.csv")
 
-df = pd.read_csv(path)
+df = pd.read_csv(file_path)
 
-print(df.head(5))
+# print(df.head(5))
 print(df.info())
-print(df.isnull().head(5))
 print(df.isnull().sum())
 print(df["age"].isnull())
 print(df["age"].isin([0,1]).all())
-print(df.describe())
+# print(df.describe())
