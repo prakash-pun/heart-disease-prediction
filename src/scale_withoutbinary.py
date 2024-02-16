4fb0b5ed9c48d5dda045dbec423bff723512034c
Swethaloyalist <swethatanikonda@loyalistcollege.com>
2024-02-16 12:36:19 -0500

    Moved merged_train_data.csv and scaled_without_bin.csv to data folder

--- src/scale_withoutbinary.py
+++ src/scale_withoutbinary.py
@@ -2,10 +2,18 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 
-data_frame = pd.read_csv("merged_train_data.csv")
+data_frame = pd.read_csv("../data/merged_train_data.csv")
+
+#assigning the columns to be excluded in a variable
+
+Z = data_frame.loc[:, data_frame.columns.isin(
+    ["id","gender", "cardio", "cholesterol", "gluc", "diabetic", "smoke", "alco", "active", "height_m", "cholesterol_1",
+     "cholesterol_2", "cholesterol_3", "gluc_1", "gluc_2", "gluc_3", "diabetic_1", "diabetic_2", "diabetic_3"])]
+
+#excluding the columns using a variable
 
 X = data_frame.loc[:, ~data_frame.columns.isin(
-    ["gender", "cardio", "cholesterol", "gluc", "diabetic", "smoke", "alco", "active", "height_m", "cholesterol_1",
+    ["id","gender", "cardio", "cholesterol", "gluc", "diabetic", "smoke", "alco", "active", "height_m", "cholesterol_1",
      "cholesterol_2", "cholesterol_3", "gluc_1", "gluc_2", "gluc_3", "diabetic_1", "diabetic_2", "diabetic_3"])]
 y = data_frame["cardio"]
 
@@ -18,6 +26,12 @@ print(scaled_data_frame.describe())
 
 scaled_data_frame['cardio'] = y
 
-scaled_data_frame.to_csv("scaled_without_bin.csv")
+#scaled_data_frame.to_csv("scaled_without_bin.csv")
+
+#adding the excluded columns in csv
+
+result_df = pd.concat([Z, scaled_data_frame], axis=1)
+
+result_df.to_csv("../data/scaled_without_bin.csv", index=False)
 
 print(scaled_data_frame.head(1))
