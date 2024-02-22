d34b9234b766eb999a889897c73a29f127520817
Prakash Pun <poonprakash22@gmail.com>
2024-02-21 21:33:45 -0500

    wip in code refactor

--- src/main.py
+++ src/main.py
@@ -2,4 +2,6 @@ from scale import scale_minmax
 
 scaled_data = scale_minmax()
 
+print("asdfasdfasdf")
+
 print(scaled_data.head()) 
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 def get_csv_file(file_name):
     current_dir = os.getcwd()
+    print("sdfsdf", current_dir)
     file_path = os.path.join(current_dir, f"data/{file_name}")
 
     return file_path
@@ -14,3 +15,6 @@ def get_data():
     df = pd.read_csv(path)
 
     return df
+
+
+print(get_csv_file("cleaned_data.csv"))
