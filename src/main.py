dce4c7611fe12b55bce65fd0bf9d530f865928a6
Prakash Pun <poonprakash22@gmail.com>
2024-03-11 17:02:40 -0400

    refractor models folder

--- src/main.py
+++ src/main.py
@@ -3,16 +3,13 @@ from feature_extraction import extract_feature
 from split_dataset import split_data
 from fill_data import fill_data
 from scale import scale_minmax
-from models.train_model import svm_model
-
-from models.random_forest import random_forest_model, random_forest_model2
-from models.train_model_logreg import LogisticRegression_model1, LogisticRegression_model2
-
+from models.train_model import TrainModel
 
 X_train, X_test, y_train, y_test = split_data()
 
 # fill and merge train dataset
-filled_x_train = fill_data(data_frame=X_train, file_name="merged_train_data.csv")
+filled_x_train = fill_data(
+    data_frame=X_train, file_name="merged_train_data.csv")
 filled_x_test = fill_data(data_frame=X_test, file_name="merged_test_data.csv")
 
 # scale
@@ -22,39 +19,21 @@ scaled_test_data = scale_minmax(filled_x_test)
 X_train = extract_feature(data_frame=scaled_train_data, y_train=y_train)
 X_test = extract_feature(data_frame=scaled_test_data, y_train=y_test)
 
-#svm scaling
 start = time.time()
-accuracy = svm_model(X_train, X_test, y_train, y_test)
+model = TrainModel(X_train, X_test, y_train, y_test)
+accuracy, f1, precision = model.svm_model()
 end = time.time()
-print("Accuracy_SVM:", accuracy)
+print("SVM Model:", accuracy, f1, precision)
 print(end-start)
-
-# logistic regression
-start2 = time.time()
-accuracy_logreg = LogisticRegression_model1(X_train, X_test, y_train, y_test)
-end2 = time.time()
-print("Accuracy_LogReg:", accuracy_logreg)
-print(end2-start2)
 
 start2 = time.time()
-precision_log = LogisticRegression_model2(X_train, X_test, y_train, y_test)
+result_lr = model.logistic_regression_model()
 end2 = time.time()
-print("Precision_LogREg:", precision_log)
+print("Logistic Regression:", result_lr)
 print(end2-start2)
 
-# random forest
 start = time.time()
-precision = random_forest_model(X_train, X_test, y_train, y_test)
+result = model.random_forest_model()
 end = time.time()
-print("Precision: ",precision)
+print("Random Forest Model: ", result)
 print(end-start)
-
-start = time.time()
-accuracy, f1_score = random_forest_model2(X_train, X_test, y_train, y_test)
-end = time.time()
-print("Accuracy_RF:", accuracy)
-print("F1_score_RF: ",f1_score)
-print(end-start)
-
-
-
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,14 +1,57 @@
 from sklearn import svm
-from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, f1_score, precision_score
 
-def svm_model(X_train, X_test, y_train, y_test):
-    clf = svm.SVC(kernel='linear', C=1.0)
-    # Train the classifier
-    clf.fit(X_train, y_train)
 
-    # Make predictions
-    predictions = clf.predict(X_test)
+class TrainModel():
 
-    # Evaluate accuracy
-    accuracy = accuracy_score(y_test, predictions)
-    return accuracy
+    def __init__(self, X_train, X_test, y_train, y_test):
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+
+    def svm_model(self):
+        clf = svm.SVC(kernel='linear', C=1.0)
+        # Train the classifier
+        clf.fit(self.X_train, self.y_train)
+
+        # Make predictions
+        predictions = clf.predict(self.X_test)
+        accuracy = accuracy_score(self.y_test, predictions)
+        f1 = f1_score(self.y_test, predictions)
+        precision = precision_score(self.y_test, predictions)
+
+        return accuracy, f1, precision
+
+    def logistic_regression_model(self):
+        clf = LogisticRegression(random_state=0)
+        # Train the classifier
+        clf.fit(self.X_train, self.y_train)
+
+        # make prediction
+        prediction = clf.predict(self.X_test)
+
+        # evaluate accuracy
+        accuracy = accuracy_score(self.y_test, prediction)
+        precision = precision_score(self.y_test, prediction)
+
+        return accuracy, precision
+
+    def random_forest_model(self):
+        # Create a Random Forest classifier
+        clf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+        # Train the classifier
+        clf.fit(self.X_train, self.y_train)
+
+        # Make predictions
+        predictions = clf.predict(self.X_test)
+
+        # Evaluate precision
+        precision = precision_score(self.y_test, predictions)
+        accuracy = accuracy_score(self.y_test, predictions)
+        f_score = f1_score(self.y_test, predictions, average="binary")
+
+        return precision, accuracy, f_score
