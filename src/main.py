00b3b77eeb879b15ac5a31b04999e4ba431b94a5
Prakash Pun <poonprakash22@gmail.com>
2024-03-22 16:28:10 -0400

    fix: generate table

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ X_train = extract_feature(data_frame=scaled_train_data, y_train=y_train)
 X_test = extract_feature(data_frame=scaled_test_data, y_train=y_test)
 
 # Train Models
-model = TrainModel(X_train , X_test , y_train, y_test)
+model = TrainModel(X_train, X_test, y_train, y_test)
 
 # Logistic Regression
 result_lr = model.logistic_regression_model()
@@ -34,13 +34,13 @@ print("XGBoost_CLF ", xg_boost)
 gbm = model.gbm_model()
 print("Gradient Boosting: ", gbm)
 
-
 metrics = {
-    "Logistic Regression": list(result_lr),
-    "XGBoost_CLF": list(xg_boost),
-    "Gradient Boosting": list(gbm),
+    "Logistic Regression Train": list(result_lr[0]),
+    "Logistic Regression Test": list(result_lr[1]),
+    "XGBoost_CLF Train": list(xg_boost[0]),
+    "XGBoost_CLF Test": list(xg_boost[1]),
+    "Gradient Boosting Train": list(gbm[0]),
+    "Gradient Boosting Test": list(gbm[1]),
 }
 
-# generate_table(metrics)
-
-
+generate_table(metrics)
