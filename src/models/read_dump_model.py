d3f97baf4a3ff5bd40b4cd3b3a33e872c3b05642
Prakash Pun <poonprakash22@gmail.com>
2024-04-03 21:18:56 -0400

    feat: model dump fix

--- src/models/read_dump_model.py
+++ src/models/read_dump_model.py
@@ -1,5 +1,8 @@
-import joblib
 import os
+import joblib
+import numpy as np
+from .train_model import metrics
+
 
 def get_dump_file(filename=""):
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -10,7 +13,81 @@ def get_dump_file(filename=""):
 
 
 def get_dump_model(filename):
-    filename = get_dump_file("logistic_model.joblib")
-    model = joblib.load(filename)
-    
+    filepath = get_dump_file(filename)
+    model = joblib.load(filepath)
+
     return model
+
+
+class DumpTrainModel():
+
+    def __init__(self, X_train, X_test, y_train, y_test):
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+
+    def logistic_regression_model(self):
+        logreg = get_dump_model("logistic_model.joblib")
+
+        # make prediction
+        train_predict = logreg.predict(self.X_train)
+
+        train_proba = logreg.predict_proba(self.X_train)
+
+        prediction = logreg.predict(self.X_test)
+        test_proba = logreg.predict_proba(self.X_test)
+
+        result_test = metrics(self.y_test, prediction, test_proba[:, 1])
+        result_train = metrics(self.y_train, train_predict, train_proba[:, 1])
+
+        # Calculate feature importances
+        feature_importance = np.abs(logreg.coef_[0])
+        feature_importance /= feature_importance.sum()
+        feature_names = self.X_train.columns.tolist()
+
+        return {"train": result_train, "test": result_test, "predict": logreg, "feature_importance": feature_importance, "feature_names": feature_names}
+
+    def xg_boost(self):
+        xgb_clf = get_dump_model("xg_boost_model.joblib")
+
+        # Make predictions
+        train_predict = xgb_clf.predict(self.X_train)
+        train_predictions_clf = (train_predict > 0.5).astype(int)
+        train_proba = xgb_clf.predict_proba(self.X_train)
+
+        predictions_clf = xgb_clf.predict(self.X_test)
+        binary_predictions_clf = (predictions_clf > 0.5).astype(int)
+        predict_proba = xgb_clf.predict_proba(self.X_test)
+
+        # Calculate metrics
+        result_train = metrics(
+            self.y_train, train_predictions_clf, train_proba[:, 1])
+        result = metrics(self.y_test, binary_predictions_clf,
+                         predict_proba[:, 1])
+
+        # Calculate feature importances
+        feature_importance = xgb_clf.feature_importances_
+        feature_names = self.X_train.columns.tolist()
+
+        return {"train": result_train, "test": result, "predict": xgb_clf, "feature_importance": feature_importance, "feature_names": feature_names}
+
+    def gbm_model(self):
+        gradient_boosting = get_dump_model("gbm_model.joblib")
+
+        # Predictions
+        train_predict = gradient_boosting.predict(self.X_train)
+        train_proba = gradient_boosting.predict_proba(self.X_train)
+
+        prediction = gradient_boosting.predict(self.X_test)
+        predict_proba = gradient_boosting.predict_proba(self.X_test)
+
+        # Calculate metrics
+        result = metrics(self.y_test, prediction, predict_proba[:, 1])
+        result_train = metrics(self.y_train, train_predict, train_proba[:, 1])
+
+        # Calculate feature importances
+        feature_importance = gradient_boosting.feature_importances_
+        feature_names = self.X_train.columns.tolist()
+
+        return {"train": result_train, "test": result, "feature_importance": feature_importance, "feature_names": feature_names}
