trying to fix scaling error

src/dashboard.py
@@ -1,33 +1,44 @@
-from split_dataset import split_data
-from fill_data import fill_data
-from scale import scale_minmax
-from sklearn.linear_model import LogisticRegression
-from feature_extraction import extract_feature
+from utils import DataInitializer
+from data_preprocessing import DataProcessor
+from feature_engineering import FeatureEngines
+from models.read_dump_model import DumpTrainModel
+import xgboost as xgb
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_auc_score, confusion_matrix, classification_report
-from utils import get_data
 import streamlit as st
 from joblib import load, dump
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import scikitplot as skplt
 from models.dash import samefeature, calculate_and_add_bmi, preprocess_input_data, encode_input_data
 
+di = DataInitializer()
+dp = DataProcessor()
+fe = FeatureEngines()
 
-data_frame = get_data("cleaned_data.csv")
+data_frame = di.get_data()
 
-X_train, X_test, y_train, y_test = split_data()
+X_train, X_test, y_train, y_test = di.split_data()
+
+# Data Increment
+X_train = pd.concat([X_train, X_train], ignore_index=False)
+y_train = pd.concat([y_train, y_train], ignore_index=False)
+
 
 # Data Filling
-filled_x_train = fill_data(data_frame=X_train)
-filled_x_test = fill_data(data_frame=X_test)
+filled_x_train = dp.fill_data(data_frame=X_train)
+filled_x_test = dp.fill_data(data_frame=X_test)
 
 # Data Scaling
-scaled_train_data = scale_minmax(filled_x_train)
-scaled_test_data = scale_minmax(filled_x_test)
+scaled_train_data = fe.scale_minmax(filled_x_train)
+scaled_test_data = fe.scale_minmax(filled_x_test)
 
 # Features
-X_train = extract_feature(data_frame=scaled_train_data, y_train=y_train)
-X_test = extract_feature(data_frame=scaled_test_data, y_train=y_test)
+X_train = fe.extract_feature(data_frame=scaled_train_data, y_train=y_train)
+train_columns = list(X_train.columns)
+X_test = scaled_test_data[train_columns]
 
 # metrics
 def metrics(y_test, predictions, proba):
@@ -39,35 +50,51 @@ def metrics(y_test, predictions, proba):
 
     return accuracy, precision, recall, f_score, roc_auc
 
-# running logistic model
-# Train the classifier
-logreg = LogisticRegression(random_state=0,max_iter=1000)
-logreg.fit(X_train, y_train)
-
-# make prediction
-train_predict = logreg.predict(X_train)
-train_proba = logreg.predict_proba(X_train)
-
-prediction = logreg.predict(X_test)
-test_proba = logreg.predict_proba(X_test)
-
-result_test = metrics(y_test, prediction, test_proba[:, 1])
-result_train = metrics(y_train, train_predict, train_proba[:, 1])
-
-print("Logistic Regression:", result_test,result_train)
-
-# saving a model
-dump(logreg,"../src/models/log_ms.model")
+# #xgboost
+# params = {
+#             'booster': ['gbtree', 'gblinear'],  # gblinear
+#             'learning_rate': np.arange(0.01, 0.9, 0.01),
+#             'n_estimators': range(50, 1000, 50),
+#             'objective': ['binary:logistic'],
+#             # ,'multi:softmax','multi:softprob','reg:logitstic'],  # Binary classification
+#             'eval_metric': ['merror', 'logloss', 'auc']  # Evaluation metric
+#             # 'subsample': np.arange(0.1, 0.9, 0.1),
+#             # 'max_depth': range(2, 7)
+#         }
+# model = xgb.XGBClassifier()
+# random_search = RandomizedSearchCV(estimator=model, param_distributions=params,n_iter=100, scoring="recall", cv=5, random_state=42, n_jobs=-1)
+#
+# random_search.fit(X_train, y_train)
+# best_param = random_search.best_params_
+#
+# # XGB CLF
+# xgb_clf = xgb.XGBClassifier(**best_param)
+# xgb_clf.fit(X_train, y_train)
+#
+# # Make predictions
+# train_predict = xgb_clf.predict(X_train)
+# train_predictions_clf = (train_predict > 0.5).astype(int)
+# train_proba = xgb_clf.predict_proba(X_train)
+#
+# predictions = xgb_clf.predict(X_test)
+# binary_predictions_clf = (predictions_clf > 0.5).astype(int)
+# predict_proba = xgb_clf.predict_proba(X_test)
+#
+# # Calculate metrics
+# result_train = metrics(y_train, train_predictions_clf, train_proba[:, 1])
+# result = metrics(y_test, binary_predictions_clf,predict_proba[:, 1])
+#
+# # # saving a model
+# dump(xgb_clf,"../src/models/xg.model")
 
 # load model
-rf_classif = load("../src/models/log_ms.model")
+rf_classif = load("../src/models/xg.model")
 
 # for checking if this is working or not
-y=rf_classif.predict(X_test)
+prediction = rf_classif.predict(X_test)
+print(prediction)
 
-print(y)
-
-st.title("heart disease Prediction :red[Prediction] :bar_chart: :chart_with_upwards_trend:")
+st.title("heart disease :red[Prediction] :bar_chart: :chart_with_upwards_trend:")
 st.markdown("Predict heart Type using different parameters")
 
 tab1, tab2, tab3 = st.tabs(["Data :clipboard:", "Performance :weight_lifter:", "Local Performance :bicyclist:"])
@@ -107,10 +134,12 @@ with tab3:
 
         input_data = pd.DataFrame([sliders], columns=data_frame.columns)
         input_data = calculate_and_add_bmi(input_data)
+        #input_data = fe.scale_minmax(input_data)
         y=preprocess_input_data(input_data, filled_x_train)
         y=encode_input_data(y, options)
 
         y = samefeature(X_train, y)
+
         print(y.to_string())
 
     with col2: